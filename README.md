cd341ec34b3ada7b19efb97b7c1a25d812007316
Briana Mancina <brianamancina@gmail.com>
2015-11-20 14:24:19 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # week-4-assessment
+# week-4-assessment
---
+++ script.js
@@ -0,0 +1,18 @@
+function()
+var animals = [
+	{name: "cow", sound:"moo"}
+	{name:"sheep", sound:"baaah"}
+	{name:"dog", sound:"arf"}
+];
+
+
+
+
+	animals.forEach(Function(animals));{
+		animal.gray = “yes”;
+}
+		console.log( “The " " + animals.name + "says" + animals.sound " " + and is gray”);
+
+});
+
+seeAndSay();
